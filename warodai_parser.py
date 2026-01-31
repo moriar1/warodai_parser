f1096c9924db78ce2304d7325123ac64bb5f3157
moriar1 <reflowcion@tuta.io>
2026-01-31 16:07:06 +0500

    style: rename classes for better reading, move comments before classes to docstrings.

--- warodai_parser.py
+++ warodai_parser.py
@@ -3,9 +3,10 @@ from dataclasses import dataclass, field
 from pathlib import Path
 
 
-# Заголовок карточки/статьи с первеводом
 @dataclass
 class Header:
+    """Заголовок карточки/статьи с первеводом"""
+
     kana: str
     kanji: str | None
     transcription: str
@@ -13,14 +14,14 @@ class Header:
     id: str
 
 
-# в README Warodai называется рубрикой
 @dataclass
-class Meaning:
+class Rubric:
+    """Рубрика с переводами, примерами, идиомами и т.д."""
+
     translation: str
-    examples: list[str] = field(default_factory=list)
+    examples: list[str] = field(default_factory=list)  # в т.ч. идиомы
 
 
-# Несколько рубрик объединённые в секции/группы (разделяются числами с точкой).
 # NOTE: иногда перед началом рубрик (или групп рубрик) может быть общий поясняющий текст,
 # как например в карточке 004-55-64, а иногда этот текст сам является
 # переводом (если в карточке нет рубрик). Этот момент реализован так:
@@ -30,38 +31,43 @@ class Meaning:
 # если в карточке затем идут секции или группы секций.
 @dataclass
 class Section:
-    meanings: list[Meaning] = field(default_factory=list)
+    """Несколько рубрик объединённые в секции/группы (разделяются числами с точкой на отдельной строке)."""
+
+    rubrics: list[Rubric] = field(default_factory=list)
 
 
-# Карточка/статья с переводом
 @dataclass
-class DictionaryEntry:
+class Entry:
+    """Карточка/статья с переводом"""
+
     header: Header
     sections: list[Section] = field(default_factory=list)
 
 
-# Основной класс хранящий все карточки/статьи с переводами
 @dataclass
 class WarodaiDictionary:
-    entries: list[DictionaryEntry] = field(default_factory=list)
+    """Основной класс хранящий все карточки/статьи с переводами"""
+
+    entries: list[Entry] = field(default_factory=list)
 
 
-text = Path("test_excerpt.txt").read_text(encoding="utf-16-le")
 header_re = re.compile(r"^([\w,…･・！ ]+)(?:【(.+)】)?\((.+)\)(?: \[(.+)\])?〔(.+)〕$")
 section_num_re = re.compile(r"^\d$")  # Например: `1` (точка опускается из-за rstrip())
 rubric_re = re.compile(r"^\d[(\. )(\) )] ")  # Например:  `1) перевод` или просто `1) `
 japanese_re = re.compile(r"^[\u3040-\u30FF\u4E00-\u9FFF]")  # яп. символы вначале строки
 
+text = Path("test_excerpt.txt").read_text(encoding="utf-16-le")
 status = 0
-dictionary_entries: list[DictionaryEntry] = []
+dictionary_entries: list[Entry] = []
 
-for card in text.split("\n\n")[1:]:  # Пропуск лицензии
+for card in text.split("\n\n")[1:]:  # Разбивка текста на карточки и пропуск лицензии
     sections: list[Section] = []
-    meanings: list[Meaning] = []
-    meaning = Meaning("")
+    rubrics: list[Rubric] = []
+    rubric = Rubric("")
     for line in card.splitlines():
         line = line.rstrip(";,. ")  # Убираем запятые разделяющие примеры и рубрики
 
+        # Заголовок всегда один
         if status == 0:
             match = header_re.match(line)
             kana, kanji, transcription, corpus, id = match.groups()
@@ -69,24 +75,26 @@ for card in text.split("\n\n")[1:]:  # Пропуск лицензии
             status = 1
             continue
 
+        # Дальше может быть: либо группа рубрик, либо рубрика с переводами и примерами
         elif status == 1:
             if section_num_re.match(line):
-                sections.append(Section(meanings))
-                meanings: list[Meaning] = []
+                # Группа рубрик
+                sections.append(Section(rubrics))
+                rubrics: list[Rubric] = []
                 continue
 
             if japanese_re.match(line):
                 # Если начинается с японских символов
                 # То есть содержит пример или идиоматическое выражение
-                meaning.examples.append(line)
+                rubric.examples.append(line)
             else:
                 # Строка содержит перевод (начало рубрики)
                 # rubric_re.sub("", line) удаляет "1) ", "1. ", но не убриает а) б)
-                meaning = Meaning(rubric_re.sub("", line))
-                meanings.append(meaning)
+                rubric = Rubric(rubric_re.sub("", line))
+                rubrics.append(rubric)
 
-    sections.append(Section(meanings))
-    dentry = DictionaryEntry(header, sections)
+    sections.append(Section(rubrics))
+    dentry = Entry(header, sections)
     dictionary_entries.append(dentry)
     status = 0
 
@@ -94,8 +102,8 @@ for entry in dictionary_entries:
     print(f"Слово: {entry.header.kana}")
     for i, section in enumerate(entry.sections):
         print(f"Секция: {i}.")
-        for j, meaning in enumerate(section.meanings, start=1):
-            print(f" Перевод {j}: {meaning.translation}")
-            for k, example in enumerate(meaning.examples, start=1):
+        for j, rubric in enumerate(section.rubrics, start=1):
+            print(f" Перевод {j}: {rubric.translation}")
+            for k, example in enumerate(rubric.examples, start=1):
                 print(f"  Пример {k}. {example}")
     print()
